792333d4941d1e6711597b52182c69c4002deea8
Justin Hui <jhui@lcs.on.ca>
2023-01-24 11:08:45 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Justin Hui on 24/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
